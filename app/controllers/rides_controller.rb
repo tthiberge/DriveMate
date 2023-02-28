d8aa325d80fe3079c04bb27127494ac1ca0a1ef3
mmkclement <maungmaungkha18290@gmail.com>
2023-02-28 14:26:50 +0100

    not delette finished yet in progress

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,14 +2,32 @@ class RidesController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def show
-
+    @ride = Ride.find(params[:id])
   end
 
   def new
-
+    @ride = Ride.new
   end
 
   def create
-    
+    @ride = Ride.new(ride_params) # to authorizse for security
+    @ride.user = current_user
+    if @ride.save
+      redirect_to ride_path(@ride)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @ride = Ride.find(param[:id])
+    @ride.destroy
+    redirect_to root_path , notice: "Ride successfully deleted.", status: :see_other
+  end
+  
+  private
+
+  def ride_params
+    params.require(:ride).permit(:car_type, :owner_presence, :seats_available, :description)
   end
 end
