c084ce32a59eb4f46736c5119a0fd2f8f6095655
Joy Danenberg <joy.danenberg@gmail.com>
2023-03-01 18:03:49 +0100

    geolocalisation

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,6 +2,6 @@ class Ride < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :departure_location
+  after_validation :geocode, if: :will_save_change_to_departure_location?
 end
