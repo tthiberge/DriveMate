4be680273deed16a45d911b6a967e52d67d59c64
Joy Danenberg <joy.danenberg@gmail.com>
2023-03-01 12:57:36 +0100

    cloudinary + navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   # Associations et validations à mettre
   has_many :bookings, dependent: :destroy
   has_many :rides
-
-
+  has_one_attached :photo
 end
