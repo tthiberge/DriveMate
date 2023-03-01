c57ee1b5c77b6dac1980c04c1ba8dc259781d4c1
Theophile Thiberge <tthiberge@gmail.com>
2023-03-01 13:15:39 +0100

    adding policies

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,19 @@
+class BookingPolicy < ApplicationPolicy
+
+  def show?
+    # Je ne comprends pas très bien pourquoi ça marche quand même sans mettre true
+    false
+  end
+
+  def create?
+    true
+  end
+
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
---
+++ app/policies/ride_policy.rb
@@ -0,0 +1,28 @@
+class RidePolicy < ApplicationPolicy
+
+  def show?
+    true
+  end
+
+  # Pas obligatoire car on fait create
+  # def new?
+  #   true
+  # end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    # Renverra true ou false selon si le propriétaire de l'instance ride est bien le current_user (appelé user par pundit)
+    (record.user == user)
+  end
+
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
