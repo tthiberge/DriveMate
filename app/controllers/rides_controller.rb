3c1772892fac23c5ce2de496b31621056eb4a4a1
Theophile Thiberge <tthiberge@gmail.com>
2023-03-01 12:52:19 +0100

    Pundit for show destroy new create de RIDES

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,14 +4,25 @@ class RidesController < ApplicationController
   def show
     @ride = Ride.find(params[:id])
     @booking = Booking.new
+    authorize @ride
+    # Je ne comprends pas très bien pourquoi ça marche quand même sans ajouter authorize @booking
+
   end
 
   def new
     @ride = Ride.new
+    # Quand même besoin de l'autoriser en variable d'instance,
+    # même si dans ride_policy je ne mets rien car ira voir create?
+    authorize @ride
   end
 
   def create
     @ride = Ride.new(ride_params) # to authorizse for security
+
+    # Pundit:
+    authorize @ride
+
+
     @ride.user = current_user
 
     if @ride.save
@@ -23,6 +34,7 @@ class RidesController < ApplicationController
 
   def destroy
     @ride = Ride.find(params[:id])
+    authorize @ride
     @ride.destroy
     redirect_to root_path, notice: "Ride successfully deleted.", status: :see_other
   end
