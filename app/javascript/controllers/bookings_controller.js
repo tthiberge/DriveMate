8b53c3a326ccce86e79380a961178b70858d0be0
Theophile Thiberge <tthiberge@gmail.com>
2023-03-02 19:39:14 +0100

    making the edit work and show form work! yihou

--- app/javascript/controllers/bookings_controller.js
+++ app/javascript/controllers/bookings_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="bookings"
 export default class extends Controller {
+  static targets = ["form"]
+
   connect() {
     console.log("hello wld");
   }
