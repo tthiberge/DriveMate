0f2a3ae485726d7f7ba4e87da14de2025ae38209
diane lesvenan <diane_lesvenan@icloud.com>
2023-03-02 14:02:18 +0100

    added PG search and its view and its style

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,22 @@
 class Ride < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  #include PgSearch::Model
+  #pg_search_scope :search_by_departure_location_and_arrival_location,
+ # against: [ :departure_location, :arrival_location ],
+  #using: {
+ #   tsearch: { prefix: true } # <-- now `superman batm` will return something!
+#  }
+#end
+
+#Ride.search_by_departure_location_and_arrival_location("Paris")
+
+  include PgSearch::Model
+  pg_search_scope :search_full_text,
+  against: {
+    departure_location: "A",
+    arrival_location:  'A',
+    description: 'B'
+  }
 end
